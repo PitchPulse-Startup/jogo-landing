39b2c80bae58a836fcc2e0e17c1467dc985d1636
israelr51 <israelram5112@gmail.com>
2026-07-06 15:28:50 -0400

    feature/postVerificationLandingPage: added landing page users are redirected to after email account confirmation

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Terms from './Terms.jsx'
 import Feedback from './Feedback.jsx'
 import DeleteAccount from './DeleteAccount.jsx'
 import Support from './Support.jsx'
+import EmailVerified from './EmailVerified.jsx'
 import './index.css'
 
 // --- ADD THESE TWO LINES ---
@@ -30,6 +31,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/feedback" element={<Feedback onBackToMain={() => window.location.href = '/'} />} />
         <Route path="/delete-account" element={<DeleteAccount onBackToMain={() => window.location.href = '/'} />} />
         <Route path="/support" element={<Support onBackToMain={() => window.location.href = '/'} />} />
+        <Route path="/email-verified" element={<EmailVerified />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
